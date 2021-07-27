56dd01d22fbad5e4253ab1e1d3758fcaa454dc2a
stillczw <czwstill@163.com>
2021-07-27 23:34:42 +0100

    updated to pass the automated test of Chapter3

--- rango/views.py
+++ rango/views.py
@@ -5,7 +5,7 @@ from django.http import HttpResponse
 # include a hyperlink to other pages by using <a></a>
 
 def index(request):
-    return HttpResponse("Rango says hey there partner!<a href = \"/rango/about\">about</a>")
+    return HttpResponse("Rango says hey there partner! <a href=\"/rango/about/\">About</a>")
 
 def about(request):
-    return HttpResponse("Rango says here is the about page.<a href = \"/rango\">index</a>")
+    return HttpResponse("Rango says here is the about page. <a href=\"/rango/\">Index</a>")
