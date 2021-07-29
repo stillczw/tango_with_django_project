4dbd4a75593d50aa97a9a1ed55f78674955ee38c
stillczw <czwstill@163.com>
2021-07-29 16:12:20 +0100

    Chapter6

--- rango/models.py
+++ rango/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.template.defaultfilters import slugify
 
 
 # Create models here.
@@ -9,9 +10,16 @@ class Category(models.Model):
     views = models.IntegerField(default=0)
     likes = models.IntegerField(default=0)
 
+    # slugify() to replace whitespace with hyphens
+    slug = models.SlugField(unique=True)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super(Category, self).save(*args, **kwargs)
+
     # fix plural typo
     class Meta:
-        verbose_name_plural = 'Categories'
+        verbose_name_plural = 'categories'
 
     def __str__(self):
         return self.name
--- rango/views.py
+++ rango/views.py
@@ -2,17 +2,44 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 
+from rango.models import Category
+from rango.models import Page
+
+
 # include a hyperlink to other pages by using <a></a>
 def index(request):
     # return HttpResponse("Rango says hey there partner! <a href=\"/rango/about/\">About</a>")
 
+    # the first 5 objects in descending order of likes
+    category_list = Category.objects.order_by('-likes')[:5]
+
     # Construct a dictionary
-    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+#   context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+    context_dict = {}
+    context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
+    context_dict['categories'] = category_list
+
     # Return a rendered response
     return render(request, 'rango/index.html', context=context_dict)
+
 
 def about(request):
     # return HttpResponse("Rango says here is the about page. <a href=\"/rango/\">Index</a>")
 
     # context_dict = {}
     return render(request, 'rango/about.html', context=None)
+
+
+def show_category(request, category_name_slug):
+    context_dict = {}
+    try:
+        category = Category.objects.get(slug=category_name_slug)
+
+        pages = Page.objects.filter(category=category)
+        context_dict['pages'] = pages
+        context_dict['category'] = category
+    except Category.DoesNotExist:
+        context_dict['category'] = None
+        context_dict['pages'] = None
+
+    return render(request, 'rango/category.html', context=context_dict)
