159eeffb42bf9968e39b75c4af8353cb4447d9af
stillczw <czwstill@163.com>
2021-07-30 03:16:24 +0100

    Ch7Exercises

--- rango/views.py
+++ rango/views.py
@@ -8,6 +8,9 @@ from rango.models import Page
 from rango.forms import CategoryForm
 from django.shortcuts import redirect
 
+from rango.forms import PageForm
+from django.urls import reverse
+
 
 # include a hyperlink to other pages by using <a></a>
 def index(request):
@@ -64,3 +67,33 @@ def add_category(request):
             print(form.errors)
 
     return render(request, 'rango/add_category.html', {'form': form})
+
+
+# add a pages in specific category page
+def add_page(request, category_name_slug):
+    # step1. get category and check if it does exist, if not, redirect to the index page
+    try:
+        category = Category.objects.get(slug=category_name_slug)
+    except Category.DoesNotExist:
+        category = None
+
+    if category is None:
+        return redirect('/rango/')
+
+    form = PageForm()
+
+    if request.method == 'POST':
+        form = PageForm(request.POST)
+        if form.is_valid():
+            if category:
+                page = form.save(commit=False)
+                page.category = category
+                page.views = 0
+                page.save()
+
+                return redirect(reverse('rango:show_category', kwargs={'category_name_slug': category_name_slug}))
+        else:
+            print(form.errors)
+
+    context_dict = {'form': form, 'category': category}
+    return render(request, 'rango/add_page.html', context=context_dict)
